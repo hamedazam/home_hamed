bebce98e95424d09aa16ad64fd1004d0b548ed63
hamedazam <168466937+hamedazam@users.noreply.github.com>
2024-04-29 21:21:17 +0300

    Add files via upload

---
+++ ha.js
@@ -0,0 +1,32 @@
+let section=document.getElementById('sec')
+console.log(section)
+let i1=document.getElementById('1')
+let i2=document.getElementById('2')
+let i3=document.getElementById('3')
+let i4=document.getElementById('4')
+let i5=document.getElementById('5')
+console.log(i1)
+console.log(i2)
+console.log(i3)
+console.log(i4)
+console.log(i5)
+i1.onclick=function()
+{
+section.style.backgroundImage='url(im1.gif)';
+}
+i2.onclick=function()
+{
+section.style.backgroundImage='url(im2.gif)';
+}
+i3.onclick=function()
+{
+section.style.backgroundImage='url(im3.gif)';
+}
+i4.onclick=function()
+{
+section.style.backgroundImage='url(im4.gif)';
+}
+i5.onclick=function()
+{
+section.style.backgroundImage='url(im5.gif)';
+}
